9686fdcce6f1a027c3c64e50bfdaa1a322716bba
AWS ParallelCluster user <ec2-user@ip-10-0-12-212.ap-southeast-1.compute.internal>
2026-09-05 18:24:04 +0000

    Implement envlist.py for managing pool of conda environments

--- envlist.py
+++ envlist.py
@@ -40,19 +40,19 @@ lock = FileLock(lock_path, timeout=time_out_secs)
 
 with lock:
     if pmode == RESET_MODE:
-        # create a list (named clist) of nevns environments with the 
+        # create a list (named clist) of nevns environments with the
         # prefix envprefix
-        # add code here
+        clist = [envprefix + str(i) for i in range(int(nenvs))]
     else:
         # load hickle file
         clist = hickle.load(file_path)
 
         if pmode == WRITE_MODE:
             # append item to end of list
-            # add code here
-        else:    
+            clist.append(env)
+        else:
             # get and remove env from clist
-            # add code here
+            env = clist.pop(0)
             # return env name
             print(env)
 
